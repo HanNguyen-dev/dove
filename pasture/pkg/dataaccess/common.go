368c7dc92ed2ad7e94f4f3f39f5459c2f029afb6
Han Nguyen <han.nguyen@outlook.com>
2024-09-15 20:20:29 -0400

    replaced mysql with postgresql

--- pasture/pkg/dataaccess/common.go
+++ pasture/pkg/dataaccess/common.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var (
 	dbuser     = os.Getenv("dbuser")
 	dbpassword = os.Getenv("dbpassword")
-	dsn        = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/jobs?charset=utf8mb4&parseTime=True&loc=Local", dbuser, dbpassword)
+	dsn        = fmt.Sprintf(
+		"host=127.0.0.1 user=%s password=%s dbname=jobs port=5432 sslmode=disable TimeZone=US/Eastern",
+		dbuser,
+		dbpassword,
+	)
 )
 
 type Repo struct {
@@ -22,7 +26,7 @@ var repo *Repo
 
 func GetInstance() *Repo {
 	if repo == nil {
-		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 		if err != nil {
 			panic(err)
